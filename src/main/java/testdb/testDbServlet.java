ca50247d5da98a0e0a94514cef612448d7a0985c
Tomo <t.gomoradzki@gmail.com>
2019-06-23 00:48:47 +0200

    Formatting code

--- src/main/java/testdb/testDbServlet.java
+++ src/main/java/testdb/testDbServlet.java
@@ -12,8 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.sql.*;
 
 /************************************************
- * Class for db testing purposes
- * Servlet implementation class testDbServlet
+ * Class for db testing purposes Servlet implementation class testDbServlet
  ************************************************/
 
 @WebServlet("/testDbServlet")
@@ -21,87 +20,74 @@ public class testDbServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		
-		//setup connection variables
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		// setup connection variables
 		String dUser = "javauser";
 		String dpass = "password";
 		String output = null;
-				
-				Statement tStatement=null;
-				Connection tConnection = null;
-				//ResultSet rs;
-				
-				try{
-					Class.forName("oracle.jdbc.driver.OracleDriver");					
-					tConnection= DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/xe",dUser,dpass);
 
-					/*	NOTE	
-					 "jdbc:oracle:thin:@localhost:1521:xe","Admin","Password"
-					 Connection URL:"jdbc:oracle:thin:@localhost:1521:xe"
-					 API:jdbc
-					 Database: oracle
-					 The type of driver:thin
-					 IP Address:@localhost
-					 Port:1521
-					 Oracle service ID: xe (enterprise eddition)
-					 */
-				
-					//Statement tStatement=tConnection.createStatement(); //create query
-					tStatement=tConnection.createStatement(); //create query
-								
-					String sql="select * from hr.CLIENT";
-					//String sqlUPDATE="UPDATE CLIENTS SET CLIENTID=4 where CLIENTID=2";
-					
-					//stT2.executeUpdate(sqlUPDATE);
-					//int a = stT2.executeUpdate(sqlUPDATE);
-					//System.out.println(a);
-					//ResultSet rs=stT1.executeQuery(sql);
-					ResultSet rs=tStatement.executeQuery(sql);
-					
-				//	
-					while(rs.next()){						
-						System.out.println(rs.getInt(1)+"  "
-										  +rs.getString(2)+"  "
-										  +rs.getString(3)+"  "
-										  +rs.getString(4)+"  "
-										  +rs.getInt(5));
-						
-						output = output + "<br>" 
-						+ rs.getInt(1)+"  "
-						+rs.getString(2)+"  "
-						+rs.getString(3)+"  "
-						+rs.getString(4)+"  "
-						+rs.getInt(5);
-					}
-					//tConnection.close();
-					
-					System.out.println("Done!");
-				
-					PrintWriter out = response.getWriter();
-					out.println("Succesful connection.<br>"+output);
-					
-				}
-				catch (SQLException e){
-					System.out.println(e);
-				}
-				catch(Exception e){
+		Statement tStatement = null;
+		Connection tConnection = null;
+		// ResultSet rs;
+
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			tConnection = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/xe", dUser, dpass);
+
+			/*
+			 * NOTE "jdbc:oracle:thin:@localhost:1521:xe","Admin","Password" Connection
+			 * URL:"jdbc:oracle:thin:@localhost:1521:xe" API:jdbc Database: oracle The type
+			 * of driver:thin IP Address:@localhost Port:1521 Oracle service ID: xe
+			 * (enterprise eddition)
+			 */
+
+			// Statement tStatement=tConnection.createStatement(); //create query
+			tStatement = tConnection.createStatement(); // create query
+
+			String sql = "select * from hr.CLIENT";
+			// String sqlUPDATE="UPDATE CLIENTS SET CLIENTID=4 where CLIENTID=2";
+
+			// stT2.executeUpdate(sqlUPDATE);
+			// int a = stT2.executeUpdate(sqlUPDATE);
+			// System.out.println(a);
+			// ResultSet rs=stT1.executeQuery(sql);
+			ResultSet rs = tStatement.executeQuery(sql);
+
+			//
+			while (rs.next()) {
+				System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3) + "  "
+						+ rs.getString(4) + "  " + rs.getInt(5));
+
+				output = output + "<br>" + rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3) + "  "
+						+ rs.getString(4) + "  " + rs.getInt(5);
+			}
+			// tConnection.close();
+
+			System.out.println("Done!");
+
+			PrintWriter out = response.getWriter();
+			out.println("Succesful connection.<br>" + output);
+
+		} catch (SQLException e) {
+			System.out.println(e);
+		} catch (Exception e) {
+			System.out.println(e);
+			// connT1.close();
+		} finally {
+			try {
+				tConnection.close();
+			} catch (SQLException e) {
 				System.out.println(e);
-				//connT1.close();
-				}
-				finally{
-					try {tConnection.close();
-					}
-					catch(SQLException e){
-						System.out.println(e);
-					}
-				}
-				
+			}
+		}
+
 	}
 
 }
